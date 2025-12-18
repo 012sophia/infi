bcb537137ddb74a0f04b4caef20e93a9490e880d
Sophia Hammermüller <HAM240106@spengergasse.at>
2025-12-18 14:25:21 +0100

    beispiele sql

--- w3schools datenbank/übungen.sql
+++ w3schools datenbank/übungen.sql
@@ -2,6 +2,9 @@ sqlite> select o.customerid,sum(od.quantity*p.price) sales from orders o
 join orderdetails od on o.orderid=od.orderid
  join products p on od.productid=p.productid group by o.customerid order by sales des
 c limit 3;
-20|35631.21
-51|23362.6
-71|22500.06
+
+Gesucht sind die CustomerID und der CustomerName aller Kunden,
+die mindestens eine Bestellung aufgegeben haben.
+Sortierung: CustomerName alphabetisch aufsteigend.
+select distinct c.customerid ,c.customername from customers c join orders o on c.customerid=o.customerid order by c.customernam
+e; 
