aa7175bbd22b26a2bf702c30e20f48669fcf6dcd
Sophia Hammermüller <HAM240106@spengergasse.at>
2026-04-09 15:08:56 +0200

    hü aufgabe 2

--- inventar datenbank/2026-03-19/ddl.sql
+++ inventar datenbank/2026-03-19/ddl.sql
@@ -3,4 +3,6 @@ CREATE TABLE products (id int primary key, name text not null,
 price int default 0.0,category_id int not null, 
 CONSTRAINT fk_Category_id FOREIGN KEY (Category_id) REFERENCES categories(id));
 
+CHECK (price >= 0);
+
 INSERT INTO products (id, name, price, category_id) VALUES (1, 'Test Product', 10, 999);
